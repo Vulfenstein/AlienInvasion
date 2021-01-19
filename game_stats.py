56d2c9a854b12436453e3040a40c92fec3de43c0
Vulfenstein <vulfenstein84@gmail.com>
2021-01-18 17:51:48 -0800

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_stats()
+        # Track high score
+        self.high_score = 0
 
         # Start game in inactive mode
         self.game_active = False
@@ -12,3 +14,4 @@ class GameStats:
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
